f41b773b1af3822dfcf51861bfcfe7b739cd3a44
sov-87 <afetisov87@gmail.com>
2015-03-04 17:52:41 +0300

    DEV-26074

--- common/src/lib/app/ViewController.js
+++ common/src/lib/app/ViewController.js
@@ -1,26 +1,77 @@
 Ext.define('Ext.lib.app.ViewController', {
 	extend : 'Ext.app.ViewController',
 	
-	loadDictionaries : function(dictionaries) {
-        var controller = this,
-            count = dictionaries.length,
-            mainView = controller.getView();
-
-        mainView.setLoading(true);
-
-        function checkLoading(val) {
-            if (val == 0) {
-                mainView.setLoading(false);
-            }
-        };
-
-        for (var i = 0; i < dictionaries.length; i++) {
-            Ext.data.StoreManager.lookup(dictionaries[i]).load(function(success) {
-                count--;
-                checkLoading(count);
-            });
-        }
-    },
+	/*
+	 * Загрузчик словарей.
+	 * dictionaries - массив. Элементом может являться строка или объект.
+	 * Строка трактуется как идентификатор хранилища 
+	 * Объект должен содержать идентификатор хранилища в качестве ключа и
+	 * функцию обратного вызова или массив в качестве значения.
+	 * callback - функция обратного вызова после загрузки всех словарей
+	 */
+	loadDictionaries : function(dictionaries, callback) {
+		var controller = this, i, properties, loader;
+		
+		loader = {
+			dictionaryCount : 0,
+			mainContainer: controller.getView(),
+			callback: callback,
+			updateDictionariesLoadingCount: function(){
+				var me = this;
+				
+				me.checkDictionariesLoading(--me.dictionaryCount);
+			},
+			checkDictionariesLoading : function(val) {
+				var me = this;
+				
+				if (val == 0) {
+					if(me.callback && ( typeof me.callback)=="function"){
+						me.callback();
+					}
+					me.mainContainer.setLoading(false);
+				}
+			},
+			load: function(dictionaries){
+				var me = this;
+				if (Array.isArray(dictionaries)) {
+					me.dictionaryCount += dictionaries.length;
+		
+					me.mainContainer.setLoading(true);
+		
+					for ( i = 0; i < dictionaries.length; i++) {
+						if (( typeof dictionaries[i]) === "string") {
+							Ext.data.StoreManager.lookup(dictionaries[i]).load(function(records, operation, success) {
+								if(success!==true && me.skipDictionaryAlert){
+									Ext.Msg.alert("Ошибка", "Ошибка при загрузке " + dictionaries[i]);
+								}
+								me.updateDictionariesLoadingCount();
+							});
+						} else {
+							me.load(dictionaries[i]);
+						}
+					}
+				} else {
+					properties = Object.getOwnPropertyNames(dictionaries);
+					if (properties.length >= 1) {
+						Ext.data.StoreManager.lookup(properties[0]).load(function(records, operation, success) {
+							if(success!==true && me.skipDictionaryAlert){
+								Ext.Msg.alert("Ошибка", "Ошибка при загрузке " + properties[0]);
+							}
+							if (( typeof dictionaries[properties[0]]) === "function") {
+								dictionaries[properties[0]](records, operation, success);
+							} else if (Array.isArray(dictionaries[properties[0]])) {
+								me.load(dictionaries[properties[0]]);
+							}
+							
+							me.updateDictionariesLoadingCount();
+						});
+					}
+				}
+			}
+		};
+		
+		loader.load(dictionaries);
+	},
     
     onError : function(msg) {
         var parser = new DOMParser(), xmlDoc = parser.parseFromString(Ext.util.Format.htmlDecode(msg), "text/xml");
