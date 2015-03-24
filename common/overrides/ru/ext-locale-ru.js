f859c53c2a8c4c7bd05ecb02f8b049628a0938a5
sov-87 <afetisov87@gmail.com>
2015-03-24 21:11:54 +0300

    DEV-26316

---
+++ common/overrides/ru/ext-locale-ru.js
@@ -0,0 +1,7 @@
+Ext.define("Ext.locale.ru.grid.RowEditor", {
+	override : "Ext.grid.RowEditor",
+	saveBtnText : 'Сохранить',
+	cancelBtnText : 'Отмена',
+	errorsText : 'Ошибки',
+	dirtyText: 'Вам необходимо сохранить изменения'
+});
