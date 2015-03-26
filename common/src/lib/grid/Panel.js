f2e3ba75f2880f54007ae7b2a99e9d47a0e84ea4
sov-87 <afetisov87@gmail.com>
2015-03-26 20:51:18 +0300

    DEV-26501

--- common/src/lib/grid/Panel.js
+++ common/src/lib/grid/Panel.js
@@ -1,11 +1,13 @@
 Ext.define('Ext.lib.grid.Panel', {
 	extend : 'Ext.grid.Panel',
 	alias : 'widget.simpleGrid',
-	
+
+	// @formatter:off
 	requires: [
 		'Ext.button.Button',
 		'Ext.grid.plugin.CellEditing',
 		'Ext.grid.plugin.RowEditing'],
+	// @formatter:on
 
 	/**
 	 * @param {Object} config Config object.
@@ -19,155 +21,138 @@ Ext.define('Ext.lib.grid.Panel', {
 	 * extraPlugins - дополнительные плагины помимо плагинов редактирования и буферизованного вывода.
 	 */
 	constructor : function(currentConfig) {
+		// @formatter:off
 		var config = {},
 			plugins,
 			buttons = [],
 			i;
-		
+		// @formatter:on
+
 		Ext.apply(config, this.getInitialConfig());
 		Ext.apply(config, currentConfig);
 		Ext.apply(config, this.cfg);
-		
+
 		plugins = config.plugins || [];
-		
-		if(config.beforeButtons!=null){
-			for(i=0; i<config.beforeButtons.length; i++){
+
+		if (config.beforeButtons != null) {
+			for ( i = 0; i < config.beforeButtons.length; i++) {
 				buttons.push(config.beforeButtons[i]);
 			}
 		}
-		
-		if(config.disableRefresh!==true){
-			buttons.push(
-				{
-					reference : 'refresh'+config.suffix,
-					icon : '/images/refresh.gif',
-					tooltip: 'Обновить',
-					handler: 'on' + config.suffix + 'Refresh'
-				}
-			);
+
+		if (config.disableRefresh !== true) {
+			buttons.push({
+				reference : 'refresh' + config.suffix,
+				icon : '/images/refresh.gif',
+				tooltip : 'Обновить',
+				handler : 'on' + config.suffix + 'Refresh'
+			});
 		}
-		if(config.disableSave!==true){
-			buttons.push(
-				{
-					reference : 'save'+config.suffix,
-					icon : '/images/save.png',
-					tooltip: 'Сохранить',
-					handler: 'on' + config.suffix + 'Save'
-				}
-			);
+		if (config.disableSave !== true) {
+			buttons.push({
+				reference : 'save' + config.suffix,
+				icon : '/images/save.png',
+				tooltip : 'Сохранить',
+				handler : 'on' + config.suffix + 'Save'
+			});
 		}
-		if(config.disableAdd!==true){
-			buttons.push(
-				{
-					reference : 'add'+config.suffix,
-					icon : '/images/add.gif',
-					tooltip: 'Добавить',
-					handler: 'on' + config.suffix + 'Add'
-				}
-			);
+		if (config.disableAdd !== true) {
+			buttons.push({
+				reference : 'add' + config.suffix,
+				icon : '/images/add.gif',
+				tooltip : 'Добавить',
+				handler : 'on' + config.suffix + 'Add'
+			});
 		}
-		if(config.disableDelete!==true){
-			buttons.push(
-				{
-					reference : 'delete'+config.suffix,
-					icon : '/images/delete.gif',
-					disabled : true,
-					tooltip: 'Удалить',
-					handler: 'on' + config.suffix + 'Delete'
-				}
-			);
+		if (config.disableDelete !== true) {
+			buttons.push({
+				reference : 'delete' + config.suffix,
+				icon : '/images/delete.gif',
+				disabled : true,
+				tooltip : 'Удалить',
+				handler : 'on' + config.suffix + 'Delete'
+			});
 		}
-		
-		if(config.afterButtons!=null){
-			for(i=0; i<config.afterButtons.length; i++){
+
+		if (config.afterButtons != null) {
+			for ( i = 0; i < config.afterButtons.length; i++) {
 				buttons.push(config.afterButtons[i]);
 			}
 		}
-		
+
 		config.dockedItems = [{
-			xtype: 'toolbar',
-			dock: config.buttonsDock || 'top',
-			items: buttons
+			xtype : 'toolbar',
+			dock : config.buttonsDock || 'top',
+			items : buttons
 		}];
-		
+
 		config.viewConfig = config.viewConfig || {
 			enableTextSelection : true
 		};
-		
-		if(config.disableEditing!==true){
-			var hasEditingPlugin=false,
-				hasBufferPlugin=false;
-			
-			for(i=0; i<plugins.length; i++){
-				if(plugins[i].ptype == 'rowediting' || plugins[i].ptype == 'cellediting'){
-					hasEditingPlugin=true;
+
+		if (config.disableEditing !== true) {
+			var hasEditingPlugin = false, hasBufferPlugin = false;
+
+			for ( i = 0; i < plugins.length; i++) {
+				if (plugins[i].ptype == 'rowediting' || plugins[i].ptype == 'cellediting') {
+					hasEditingPlugin = true;
 					break;
 				}
 			}
-			if(!hasEditingPlugin){
-				plugins.push(
-					(config.editing=='row')?
-					{
-						ptype: 'rowediting',
-						clicksToEdit : 2
-					}:
-					{
-						ptype: 'cellediting',
-						clicksToEdit : 1
-					}
-				);
+			if (!hasEditingPlugin) {
+				plugins.push((config.editing == 'row') ? {
+					ptype : 'rowediting',
+					clicksToEdit : 2
+				} : {
+					ptype : 'cellediting',
+					clicksToEdit : 1
+				});
 			}
 		}
-		
-		if(config.enableBuffering===true){
-			var hasBufferPlugin=false;
-			
-			for(i=0; i<plugins.length; i++){
-				if(plugins[i].ptype == 'bufferedrenderer'){
-					hasBufferPlugin=true;
+
+		if (config.enableBuffering === true) {
+			var hasBufferPlugin = false;
+
+			for ( i = 0; i < plugins.length; i++) {
+				if (plugins[i].ptype == 'bufferedrenderer') {
+					hasBufferPlugin = true;
 					break;
 				}
 			}
-			if(!hasBufferPlugin){
-				plugins.push(
-					{
-						ptype: 'bufferedrenderer',
-						trailingBufferZone: 20,
-						leadingBufferZone: 50
-					}
-				);
+			if (!hasBufferPlugin) {
+				plugins.push({
+					ptype : 'bufferedrenderer',
+					trailingBufferZone : 20,
+					leadingBufferZone : 50
+				});
 			}
 		}
-		
-		if(config.extraPlugins!=null){
-			for(i=0; i<config.extraPlugins.length; i++){
-				if(
-					config.extraPlugins[i].ptype!='rowediting' &&
-					config.extraPlugins[i].ptype!='cellediting' &&
-					config.extraPlugins[i].ptype!='bufferedrenderer'
-				){
+
+		if (config.extraPlugins != null) {
+			for ( i = 0; i < config.extraPlugins.length; i++) {
+				if (config.extraPlugins[i].ptype != 'rowediting' && config.extraPlugins[i].ptype != 'cellediting' && config.extraPlugins[i].ptype != 'bufferedrenderer') {
 					plugins.push(config.extraPlugins[i]);
 				}
 			}
 		}
-		
-		for(i=0; i<plugins.length; i++){
-			if(plugins[i].pluginId==null){
-				plugins[i].pluginId = plugins[i].ptype+config.suffix;
+
+		for ( i = 0; i < plugins.length; i++) {
+			if (plugins[i].pluginId == null) {
+				plugins[i].pluginId = plugins[i].ptype + config.suffix;
 			}
 		}
-		
+
 		config.plugins = plugins;
-		
-		config.reference = config.suffix+'Table';
-		
-		if(config.disableDeleteColumn!==true){
+
+		config.reference = config.suffix + 'Table';
+
+		if (config.disableDeleteColumn !== true) {
 			config.columns.push({
-				xtype:'actioncolumn',
-				width:20,
-				icon: '/ext/examples/shared/icons/fam/cross.gif',
-				tooltip: 'Удалить',
-				handler: function(grid, rowIndex){
+				xtype : 'actioncolumn',
+				width : 20,
+				icon : '/ext/examples/shared/icons/fam/cross.gif',
+				tooltip : 'Удалить',
+				handler : function(grid, rowIndex) {
 					grid.store.removeAt(rowIndex);
 				}
 			});
@@ -176,5 +161,58 @@ Ext.define('Ext.lib.grid.Panel', {
 		Ext.apply(this, config);
 
 		this.callParent(arguments);
-	}
+	},
+
+	getRecsFromCsv : function(csv) {
+		// @formatter:off
+        var grid = this,
+        	rows = csv.split("\n"),
+        	record,
+        	records = [],
+        	columns = grid.columns,
+        	j, cols, l;
+        // @formatter:on
+
+		rows.every(function(row) {
+			if (row.length > 0) {
+				cols = row.split("\t");
+				record = {};
+				l = Math.min(cols.length, columns.length);
+
+				for ( j = 0; j < l; j++) {
+					if (columns[j].config.field.xtype === "numberfield") {
+						cols[j] = cols[j].replace(new RegExp(String.fromCharCode(160), "g"), "").replace(/,/g, ".");
+					};
+					record[columns[j].dataIndex] = cols[j];
+				}
+
+				records.push(record);
+			}
+
+			return true;
+		});
+
+		return records;
+	},
+
+	getCsvDataFromRecs : function() {
+		// @formatter:off
+        var grid = this,
+        	store = grid.store,
+        	rows = [];
+        // @formatter:on
+
+		grid.getSelectionModel().getSelection().every(function(record) {
+			var row = [];
+			grid.columns.every(function(column) {
+				row.push(record.get(column.dataIndex));
+				
+				return true;
+			});
+			rows.push(row.join("\t"));
+			
+			return true;
+		});
+		return rows.length>0 ? rows.join("\n") : null;
+	},
 });
