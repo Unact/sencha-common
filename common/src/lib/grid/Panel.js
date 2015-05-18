fc7ffb570659a578ee21233d723510688a960a01
kurtov <kurtov@unact.ru>
2015-05-18 09:16:49 +0300

    Ext.lib.ErrorMsg

--- common/src/lib/grid/Panel.js
+++ common/src/lib/grid/Panel.js
@@ -322,7 +322,7 @@ Ext.define('Ext.lib.grid.Panel', {
 				
 				failure: function(batch, opt){
 					if(batch.exceptions.length>0){
-						grid.errorMsg('Ошибка сохранения', batch.exceptions);
+						Ext.lib.ErrorMsg.show('Ошибка сохранения', batch.exceptions);
 					};
 					grid.setLoading(false);
 				}
@@ -384,35 +384,10 @@ Ext.define('Ext.lib.grid.Panel', {
 					if(model && (model = store.getById(model.getId())))
 						sm.select(model);
 				} else
-					Ext.Msg.alert('Ошибка', operation.getError().responseText);
+					Ext.lib.ErrorMsg.show('Ошибка получения данных', operation);
 
 				grid.setLoading(false);		
 			}
 		});
-	},
-	
-	errorMsg: function(msg, operations) {
-		var err, tail = "", body, bodyDetail = [];
-		if(operations instanceof String) {
-			err = operations; 
-		} else {
-			if(operations instanceof Array) {
-				err = operations[0].getError();
-				tail = '<br/>Всего ошибок: ' + operations.length;
-			} else {
-				err = operations.getError();
-			}
-		}
-		
-		if(err instanceof String) {
-			body = err;
-		} else {
-			for (var key in err) {
-				bodyDetail.push(key + " = " + err[key]);
-			}
-			body = bodyDetail.join("<br/>");
-		}
-		
-		Ext.Msg.alert('Ошибка', msg + "<br/>" + body + tail);
 	}
 });
