1eaef7c1e9ae29e7094a283c6cb7a08dade2a942
sov-87 <afetisov87@gmail.com>
2015-06-28 17:02:28 +0300

    add token to connection

--- common/overrides/data/Connection.js
+++ common/overrides/data/Connection.js
@@ -11,5 +11,20 @@ Ext.define('Ext.overrides.data.Connection', {
 				window.location.replace('/');
 			}
 		});
+	},
+	
+	/**
+	 * Sets various options such as the url, params for the request
+	 * @param {Object} options The initial options
+	 * @param {Object} scope The scope to execute in
+	 * @return {Object} The params for the request
+	 */
+	setOptions : function(options, scope) {
+		var me = this;
+		var res = me.callParent(arguments);
+		
+		res.url = Ext.urlAppend(res.url, Ext.Object.toQueryString({authenticity_token: window._token}));
+
+		return res;
 	}
-}); 
+});
