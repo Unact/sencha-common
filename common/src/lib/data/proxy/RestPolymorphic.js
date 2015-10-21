8437fe26940cc7785abb9d153b16508a18bec0b4
kurtov <kurtov@unact.ru>
2015-10-21 17:58:25 +0300

    RestPolymorphic

---
+++ common/src/lib/data/proxy/RestPolymorphic.js
@@ -0,0 +1,17 @@
+Ext.define('Ext.lib.data.proxy.RestPolymorphic', {
+    extend: 'Ext.data.proxy.Rest',
+    alias : 'proxy.restpolymorphic',
+    
+    buildUrl : function (request) {
+        var url = this.getUrl(request);
+        var params = request.getParams() || {};
+
+        url = '/' + params.polymorphic_type + '/' + params.polymorphic_id + '/' + url;
+        
+        delete params.polymorphic_type;
+        delete params.polymorphic_id;
+        
+        request.setUrl(url);
+        return this.callParent([request]);
+    }
+});
