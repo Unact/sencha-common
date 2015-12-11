23930b186a69ab377d7df322b36d5389fc22f805
sov-87 <afetisov87@gmail.com>
2015-12-11 11:39:46 +0300

    DEV-28735

---
+++ common/src/lib/data/proxy/RestJsonLimitless.js
@@ -0,0 +1,12 @@
+Ext.define('Ext.lib.data.proxy.RestJsonLimitless', {
+	extend : 'Ext.data.proxy.Rest',
+	alias : 'proxy.restjsonlimitless',
+
+	limitParam : '',
+	pageParam : '',
+
+	headers : {
+		'Accept': 'application/json',
+		'Content-Type' : 'application/json'
+	}
+}); 
