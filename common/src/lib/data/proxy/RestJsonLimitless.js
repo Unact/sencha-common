1bd99a2547ff23a680a6d25ca242bb554c894ac1
sov-87 <afetisov87@gmail.com>
2016-01-25 14:43:12 +0300

    DEV-29095. improve restjsonlimitless

--- common/src/lib/data/proxy/RestJsonLimitless.js
+++ common/src/lib/data/proxy/RestJsonLimitless.js
@@ -5,8 +5,22 @@ Ext.define('Ext.lib.data.proxy.RestJsonLimitless', {
 	limitParam : '',
 	pageParam : '',
 	
+	extraParams: {
+		limitless: true
+	},
+	
 	headers : {
 		'Accept': 'application/json',
 		'Content-Type' : 'application/json'
+	},
+	
+	constructor: function(config){
+		var me = this;
+		if(!config.extraParams){
+			config.extraParams = {};
+		};
+		config.extraParams.limitless = true;
+		
+		me.callParent(arguments);
 	}
 }); 
