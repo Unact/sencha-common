519793f162be6ea5357c8664a5074a6943367d47
sov-87 <afetisov87@gmail.com>
2016-03-29 18:11:58 +0300

    DEV-29623

--- common/overrides/data/field/Date.js
+++ common/overrides/data/field/Date.js
@@ -2,5 +2,6 @@ Ext.define('Ext.overrides.data.field.Date', {
 	override : 'Ext.data.field.Date',
 
 	dateFormat: 'c',
-	dateReadFormat: 'c'
+	dateReadFormat: 'c',
+	dateWriteFormat: 'Y-m-d\\TH:i:s'
 }); 
