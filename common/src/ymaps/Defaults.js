d030365f95fc89cd59102298cd15fbf6d929a7bb
DCrow <d_dcrow@outlook.com>
2016-04-13 16:23:06 +0300

    DEV-29294 map defaults

--- common/src/ymaps/Defaults.js
+++ common/src/ymaps/Defaults.js
@@ -1,6 +1,8 @@
 Ext.define('Renew.ymaps.Defaults', {
 	statics: {
 		center: [55.752767, 37.622642],
-		zoom: 13
+		zoom: 13,
+        iconImageSize: [37, 43],
+        iconImageOffset: [-11, -43]
 	}
 });
