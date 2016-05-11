cfb65fac8aa24f15954e37290776fbc04513baf9
kurtov <kurtov@unact.ru>
2016-05-11 15:08:21 +0300

    DEV-29983

--- common/overrides/data/AbstractStore.js
+++ common/overrides/data/AbstractStore.js
@@ -1,64 +1,68 @@
 Ext.define('Ext.overrides.data.AbstractStore', {
-	override : 'Ext.data.AbstractStore',
+    override: 'Ext.data.AbstractStore',
 
-	hasChanges : function() {
-		var me = this;
-		var changed = (Ext.getVersion().isLessThan('6')) ?
-			me.getUnfiltered().createFiltered(me.filterHasChanges).getRange().length>0 :
-			me.filterDataSource(me.filterHasChanges).length>0;
-		
-		return changed || me.getRemovedRecords().length > 0;
-	},
-	
-	filterHasChanges: function(item) {
-		return item.phantom === true || item.dirty === true;
-	},
-	
-	getValidations: function(){
-		var me = this,
-			validations = [],
-			differentValidations = [],
-			i, j, different;
-		
-		me.each(function(record){
-			var validation = record.getValidation();
-			if(validation.dirty && (record.dirty || record.phantom)) {
-				validations.push(validation.data);
-			}
-		});
-		
-		for(i = 0; i<validations.length; i++){
-			different = true;
-			for(j = 0; j<differentValidations.length && different; j++){
-				if(Ext.Object.equals(validations[i], differentValidations[j])){
-					different = false;
-				}
-			}
-			if(different){
-				differentValidations.push(validations[i]);
-			}
-		}
-		
-		return differentValidations;
-	},
-	
-	getValidationMessages: function(){
-		var me = this,
-			validations = me.getValidations(),
-			messages = [],
-			message,
-			i, field;
-		
-		for(i = 0; i<validations.length; i++){
-			message = {};
-			for(field in validations[i]){
-				if(validations[i][field]!==true){
-					message[field] = validations[i][field];
-				}
-			}
-			messages.push(message);
-		}
-		
-		return messages;
-	}
-}); 
+    hasChanges : function() {
+        var me = this;
+        var changed = (Ext.getVersion().isLessThan('6')) ?
+            me.getUnfiltered().createFiltered(me.filterHasChanges).getRange().length>0 :
+            me.filterDataSource(me.filterHasChanges).length>0;
+
+        return changed || me.getRemovedRecords().length > 0;
+    },
+
+    filterHasChanges: function(item) {
+        return item.phantom === true || item.dirty === true;
+    },
+
+    getValidations: function(){
+        var me = this,
+            validations = [],
+            differentValidations = [],
+            i, j, different;
+
+        me.each(function(record){
+            var validation = record.getValidation();
+            if(validation.dirty && (record.dirty || record.phantom)) {
+                validations.push(validation.data);
+            }
+        });
+
+        for(i = 0; i<validations.length; i++){
+            different = true;
+            for(j = 0; j<differentValidations.length && different; j++){
+                if(Ext.Object.equals(validations[i], differentValidations[j])){
+                    different = false;
+                }
+            }
+            if(different){
+                differentValidations.push(validations[i]);
+            }
+        }
+
+        return differentValidations;
+    },
+
+    getValidationMessages: function(){
+        var me = this,
+            validations = me.getValidations(),
+            messages = [],
+            message,
+            i, field;
+
+        for(i = 0; i<validations.length; i++){
+            message = {};
+            for(field in validations[i]){
+                if(validations[i][field]!==true){
+                    message[field] = validations[i][field];
+                }
+            }
+            messages.push(message);
+        }
+
+        return messages;
+    },
+
+    findExactRecord: function(property, value, startIndex) {
+        return this.findRecord(property, value, startIndex, false, true, true);
+    }
+});
