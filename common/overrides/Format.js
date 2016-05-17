08fc18abebd4ea547a1e580a47d9c2e4934dfd8b
sov-87 <afetisov87@gmail.com>
2016-05-17 16:19:57 +0300

    DEV-30019. fix lang and date format

--- common/overrides/Format.js
+++ common/overrides/Format.js
@@ -1,19 +1,19 @@
 Ext.onReady(function() {
-    Ext.override(Ext.util.Format, {
-       originalNumberFormatter: Ext.util.Format.number,
-        number: function(v, formatString) {
-            if (v < 0) {
-                //negative number: flip the sign, format then prepend '-' onto output
-                return '-' + this.originalNumberFormatter(v * -1, formatString);
-            } else {
-                //positive number: as you were
-                return this.originalNumberFormatter(v, formatString);
-            }
-        },
-        percent: function(v, formatString) {
-            return Ext.util.Format.number(v*100, '0,0.0 %');
-        }
-    }
-    );
+	Ext.override(Ext.util.Format,
+	{
+		thousandSeparator: ' ',
+		originalNumberFormatter : Ext.util.Format.number,
+		number : function(v, formatString) {
+			if (v < 0) {
+				//negative number: flip the sign, format then prepend '-' onto output
+				return '-' + this.originalNumberFormatter(v * -1, formatString);
+			} else {
+				//positive number: as you were
+				return this.originalNumberFormatter(v, formatString);
+			}
+		},
+		percent : function(v, formatString) {
+			return Ext.util.Format.number(v * 100, '0,0.0 %');
+		}
+	});
 });
-
---
+++ common/overrides/lang-ru.js
@@ -0,0 +1,38 @@
+/* ***********************************************************
+ * Доопределяем руссификацию из /ext/locale/ext-lang-ru.js
+ *
+ * Причина:
+ * в ext-lang-ru.js кривая руссификация. календарь по-прежнему начинается с воскресенья.
+ * ext-lang-ru.js менять не будем, может, в будущих версиях они исправят эту багу.
+ *
+ ************************************************************/
+Ext.onReady(function() {
+	Ext.override(
+    	Ext.form.field.Date,
+    	{
+			startDay : 1,		//Начало недели - понедельник
+			format : 'd.m.Y',	//Год записывается четырьмя знаками
+			
+			statics: {
+				dateWithTimeFormat: 'd.m.Y H:i'
+			}
+		}
+	);
+	
+	Ext.define("app.locale.ru.form.field.Time", {                 //в ext-lang-ru.js вообще отсутствует время
+		override: "Ext.form.field.Time",
+		format: "H:i",
+		minText: "Время в этом поле должно быть позже {0}",
+		maxText: "Время в этом поле должно быть раньше {0}",
+		invalidText: "{0} не является правильным временем"
+	});
+	
+	Ext.define("Ext.locale.ru.form.field.Number", {
+		override: "Ext.form.field.Number",
+		decimalSeparator: ","
+	});
+	
+	Ext.override(Ext.Date, {
+		defaultFormat: 'd.m.Y'
+	});
+});
