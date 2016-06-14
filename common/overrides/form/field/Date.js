e6a03750ea18ef1f9840cb317483fea77c79eb6a
kurtov <kurtov@unact.ru>
2016-06-14 11:00:51 +0300

    DEV-30317
    Ошибка в Ретро-бонусах

--- common/overrides/form/field/Date.js
+++ common/overrides/form/field/Date.js
@@ -1,11 +1,11 @@
+Ext.form.field.Date.defaultDateFormat = 'd.m.Y';
 Ext.define('Ext.overrides.form.field.Date', {
-	override : 'Ext.form.field.Date',
+    override: 'Ext.form.field.Date',
 
-	startDay : 1,		//Начало недели - понедельник
-	format : Ext.form.field.Date.defaultDateFormat,	//Год записывается четырьмя знаками
-	
-	statics: {
-	    defaultDateFormat: 'd.m.Y',
-		dateWithTimeFormat: 'd.m.Y H:i'
-	}
+    statics: {
+        dateWithTimeFormat: 'd.m.Y H:i'
+    },
+
+    startDay: 1,                                  //Начало недели - понедельник
+    format: Ext.form.field.Date.defaultDateFormat //Год записывается четырьмя знаками
 });
