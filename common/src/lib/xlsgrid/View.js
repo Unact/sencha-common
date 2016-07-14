43c2ec56319297bdfa7d7bdda6b401352c5f0e83
radik6x6gr <radik6x6gr@users.noreply.github.com>
2016-07-14 16:19:04 +0300

    DEV-29882

--- common/src/lib/xlsgrid/View.js
+++ common/src/lib/xlsgrid/View.js
@@ -8,24 +8,28 @@ Ext.define('Ext.lib.xlsgrid.View', {
 
     /*
         Окно с полем XLS, в которое вставляется CSV.
+
         В конфигах колонок (cfg.columns[]), которые будут в XLS, нужно указать:
             1. columnInXls (обязательно) - номер колонки в CSV
-            2. identificator - поле является идентификатором
+            2. identificator (обязательно) - идентификатор - целое число
             3. required - поле является обязательным
-        Должен быть один идентификатор - целое число, уникальность значений не обязательна.
-        В ViewController нужно определить метод getRequestOptions, нужен для загрузки данных из БД о вставленных строках.
-        Метод возвращает объект с полями:
+        Уникальность идентификатора задается конфигом uniqueByIdentificator; если он false, то уникальность по всей записи.
+        В ViewController нужно определить методы getRequestOptions и getSaveOptions.
+        Метод должен возвращать объект с полями:
             1. url
             2. method
             3. timeout
             4. params
+            5. jsonData (в getSaveOptions)
     */
 
-    init: function(currentConfig) {
-        this.callParent(arguments);
+    listeners: {
+        viewready: 'onViewReady',
+        scope: 'controller'
     },
 
     config: {
+        uniqueByIdentificator: true,
         autoEditOnAdd: 0,
         disableDeleteColumn: true,
         afterButtons: [{
--- common/src/lib/xlsgrid/ViewController.js
+++ common/src/lib/xlsgrid/ViewController.js
@@ -9,16 +9,69 @@ Ext.define('Ext.lib.xlsgrid.ViewController', {
     repeated: [],
     invalid: [],
 
-    init: function() {
+    onViewReady: function() {
         var me = this;
+        var view = me.getView();
 
-        me.getView().getColumns().forEach(function(column) {
+        view.getColumns().forEach(function(column) {
             if (column.identificator) {
                 me.idColumn = column.dataIndex;
             }
         });
 
-        me.callParent(arguments);
+        view.findPlugin('cellediting').on({
+            validateedit: 'onValidateEdit',
+            scope: me
+        });
+    },
+
+    getEditableColumns: function() {
+        return this.getView().getColumns().filter(function(column) {
+            return (column.editor || column.field);
+        }).map(function(column) {
+            return column.dataIndex;
+        });
+    },
+
+    onSave: function() {
+        var me = this;
+        var view = me.getView();
+        var store = view.getStore();
+        var gridData = [];
+        var errorText = me.getValidationText();
+        var saveOptions;
+        var columns = me.getEditableColumns();
+
+        if (store.count() === 0) {
+            return;
+        }
+
+        view.setLoading(true);
+
+        if (errorText.length) {
+            Ext.Msg.alert('Ошибка!', errorText);
+        } else {
+            if (me.beforeSave && !me.beforeSave()) {
+                return;
+            }
+            store.each(function(r) {
+                var dataObject = {};
+                columns.forEach(function(c) {
+                    dataObject[c] = r.get(c);
+                });
+                gridData.push(dataObject);
+            });
+
+            saveOptions = me.getSaveOptions(gridData);
+            saveOptions.callback = function(options, success, response) {
+                view.setLoading(false);
+                if (success) {
+                    Ext.Msg.alert('Сообщение', 'Данные успешно сохранены')
+                    store.commitChanges();
+                }
+            };
+            Ext.Ajax.request(saveOptions);
+        }
     },
 
     onClearButtonClick: function() {
@@ -26,14 +79,71 @@ Ext.define('Ext.lib.xlsgrid.ViewController', {
     },
 
     onRefresh: function() {
-        Ext.Msg.alert('Ok', 'Refreshed');
+        var me = this;
+        var store = me.getView().getStore();
+        var ids = [];
+        var storeRecords = [];
+
+        if (store.count() === 0) {
+            return;
+        }
+
+        store.each(function(r) {
+            ids.push(r.get(me.idColumn));
+            storeRecords.push(r.getData());
+        });
+
+        me.loadRemoteData(ids, function(receivedRecords) {
+            me.mergeRecordArrays(receivedRecords, storeRecords).forEach(function(r) {
+                store.each(function(model) {
+                    if (r[me.idColumn] == model.get(me.idColumn)) {
+                        Object.keys(r).forEach(function(key) {
+                            model.set(key, r[key]);
+                        });
+                    }
+                });
+            });
+        });
     },
 
-    onSave: function() {
+    onValidateEdit: function(editor, context) {
+        var me = this;
+        var value = Number(context.value);
+
+        if (context.field === me.idColumn) {
+            if (context.value === '' || value === context.originalValue) {
+                return false;
+            } else {
+                me.loadRemoteData([value], function(receivedRecords, receivedIds) {
+                    var store = me.getView().getStore();
+                    store.each(function(r) {
+                        if (r.isModified(me.idColumn)) {
+                            var model = store.findExactRecord(me.idColumn, value);
+                            var record = me.mergeRecordArrays(receivedRecords, [model.getData()])[0];
+                            if (receivedIds.length) {
+                                Object.keys(record).forEach(function(key) {
+                                    model.set(key, record[key]);
+                                });
+                                model.commit();
+                            } else {
+                                store.remove(model);
+                            }
+                            return false;
+                        }
+                    });
+                });
+            }
+        }
+
+        return true;
+    },
+
+    getValidationText: function() {
         var me = this;
         var view = me.getView();
         var store = view.getStore();
         var messageLines = [];
+        var repeated = [];
 
         store.each(function(r) {
             var validation = r.getValidation();
@@ -49,8 +159,33 @@ Ext.define('Ext.lib.xlsgrid.ViewController', {
                 messageLines.push((id ? String(id) + ': ' : '') + messages.join(', '));
             }
         });
+
+        store.each(function(outer) {
+            var _values = function(model) {
+                var columns = me.getEditableColumns();
+                return columns.map(function(key) {return model.get(key); });
+            };
+            var outerValues = _values(outer);
+            var found = store.findBy(function(inner) {
+                var innerValues = _values(inner);
+                return (outer.getId() !== inner.getId() &&
+                    ((Ext.Array.equals(outerValues, innerValues) && !view.uniqueByIdentificator) ||
+                    (outer.get(me.idColumn) === inner.get(me.idColumn) && view.uniqueByIdentificator))
+                );
+            });
+            if (found > -1) {
+                repeated.push(outer.get(me.idColumn));
+            }
+        });
+
+        if (repeated.length) {
+            messageLines.push('Найдены повторяющиеся строки: ' + Ext.Array.unique(repeated).join(','));
+        }
+
         if (messageLines.length) {
-            Ext.Msg.alert('Ошибка!', messageLines.join('<br/>'));
+            return messageLines.join('<br/>');
+        } else {
+            return '';
         }
     },
 
@@ -60,15 +195,15 @@ Ext.define('Ext.lib.xlsgrid.ViewController', {
         var idColumnText = Ext.Array.findBy(me.getView().getColumns(), function(item) {
             return item.dataIndex === me.idColumn;
         }).text;
+        var addToMsg = function(items, reason) {
+            if (items && items.length) {
+                msg.push(reason + ' значения поля "' + idColumnText + '": ' +
+                    Ext.Array.unique(items).join(', '));
+            }
+        };
 
-        if (me.invalid && me.invalid.length) {
-            msg.push('Несуществующие значения поля "' + idColumnText + '": ' +
-                Ext.Array.unique(me.invalid).join(', '));
-        }
-        if (me.repeated && me.repeated.length) {
-            msg.push('Повторяющиеся значения поля "' + idColumnText + '": ' +
-                Ext.Array.unique(me.repeated).join(', '));
-        }
+        addToMsg(me.invalid, 'Несуществующие');
+        addToMsg(me.repeated, 'Повторяющиеся');
 
         me.repeated = [];
         me.invalid = [];
@@ -78,7 +213,7 @@ Ext.define('Ext.lib.xlsgrid.ViewController', {
         }
     },
 
-    mergeObjectArrays: function(arr1, arr2) {
+    mergeRecordArrays: function(arr1, arr2) {
         var me = this;
         var fields = me.getView().getColumns().map(function(column) {return column.dataIndex;});
         var result = [];
@@ -98,56 +233,90 @@ Ext.define('Ext.lib.xlsgrid.ViewController', {
         return result;
     },
 
-    loadRemoteData: function(records) {
+    loadRemoteData: function(records, callback) {
         var me = this;
         var view = me.getView();
         var store = view.getStore();
-        var ids = records.map(function(r) { return Number(r[me.idColumn]); });
+        var xlsRecords = typeof records[0] === "object"; // данные из поля XLS или список с id
+        var ids = xlsRecords ?
+            records.map(function(r) {
+                return Number(r[me.idColumn]);
+            }) : records;
         var requestOptions = me.getRequestOptions(ids);
 
-        view.setLoading(true);
+        if (records.length) {
+            view.setLoading(true);
 
-        requestOptions.headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        };
+            requestOptions.headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            };
 
-        requestOptions.success = function(response) {
-            var receivedRecords = Ext.JSON.decode(response.responseText, true);
-            var receivedIds = receivedRecords.map(function(r) { return Number(r[me.idColumn]); });
-            me.invalid = me.invalid.concat(Ext.Array.difference(ids, receivedIds));
-            store.add(me.mergeObjectArrays(records, receivedRecords));
-            view.setLoading(false);
+            requestOptions.callback = function(options, success, response) {
+                if (success) {
+                    var receivedRecords = Ext.JSON.decode(response.responseText, true);
+                    var receivedIds = receivedRecords.map(function(r) { return Number(r[me.idColumn]); });
+                    me.invalid = me.invalid.concat(Ext.Array.difference(ids, receivedIds));
+                    callback.call(this, receivedRecords, receivedIds);
+                    me.showMessage();
+                } else {
+                    Ext.Msg.alert('Ошибка', response.responseText);
+                }
+                view.setLoading(false);
+            };
+
+            Ext.Ajax.request(requestOptions);
+        } else {
             me.showMessage();
-        };
-
-        Ext.Ajax.request(requestOptions);
+        }
     },
 
-    isDublicate: function(records, record) {
-        var store = this.getView().getStore();
-        var found = Ext.Array.findBy(records, function(r) {
-            return Ext.Array.equals(Ext.Object.getValues(record), Ext.Object.getValues(r));
-        });
+    unique: function(records) {
+        var me = this;
+        var view = me.getView();
+        var uniqueById = view.uniqueByIdentificator;
+        var store = view.getStore();
+        var result = [];
 
-        if (found !== null) {
-            return true;
+        for (var i = records.length - 1; i >= 0; i--) {
+            var rec = records[i];
+            var found = Ext.Array.findBy(result, function(r) {
+                return uniqueById ?
+                    rec[me.idColumn] === r[me.idColumn] :
+                    Ext.Array.equals(Ext.Object.getValues(rec), Ext.Object.getValues(r));
+            });
+
+            if (found !== null) {
+                me.repeated.push(rec[me.idColumn]);
+                continue;
+            }
+
+            found = store.findBy(function(r) {
+                if (uniqueById) {
+                    return rec[me.idColumn] == r.get(me.idColumn);
+                } else {
+                    for (var i in rec) {
+                        var storeValue = r.get(i);
+                        if (storeValue instanceof Date) {
+                            storeValue = Ext.Date.format(storeValue, 'd.m.Y');
+                        }
+                        if (storeValue != rec[i]) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            });
+
+            if (found > -1) {
+                me.repeated.push(rec[me.idColumn]);
+                continue;
+            }
+
+            result.splice(0, 0, rec);
         }
 
-        found = store.findBy(function(r) {
-            for (var i in record) {
-                var storeValue = r.get(i);
-                if (storeValue instanceof Date) {
-                    storeValue = Ext.Date.format(storeValue, 'd.m.Y');
-                }
-                if (storeValue != record[i]) {
-                    return false;
-                }
-            }
-            return true;
-        });
-
-        return found > -1;
+        return result;
     },
 
     extractRecordsFromXls: function(xls) {
@@ -174,12 +343,13 @@ Ext.define('Ext.lib.xlsgrid.ViewController', {
             var row = row.split('\t');
             var record = {};
             if (row.length) {
+                var id;
                 for (var i in colSpecs) {
                     var item = row[i] && row[i].length ? row[i] : null;
                     var columnStore = colSpecs[i].columnStore;
 
                     if (columnStore) {
-                        var recordFromCombo = columnStore.getAt(columnStore.findExact('name', item));
+                        var recordFromCombo = columnStore.findExactRecord('name', item);
                         item = recordFromCombo ? recordFromCombo.get('id') : null;
                     }
 
@@ -189,22 +359,23 @@ Ext.define('Ext.lib.xlsgrid.ViewController', {
 
                     record[colSpecs[i].dataIndex] = item;
                 }
-                if (me.isDublicate(records, record)) {
-                    me.repeated.push(record[me.idColumn]);
+                id = record[me.idColumn];
+                if (Ext.Number.from(id, null) === null) {
+                    me.invalid.push(id);
                 } else {
                     records.push(record);
                 }
             }
         });
 
-        return records;
+        return me.unique(records);
     },
 
-    onXlsChanged: function(field, xls, oldValue) {
+    onXlsChanged: function(field, xls) {
         var me = this;
         var records;
 
-        if (!xls || xls.length === 0) {
+        if (!xls || !xls.length) {
             return;
         }
 
@@ -212,6 +383,8 @@ Ext.define('Ext.lib.xlsgrid.ViewController', {
 
         records = me.extractRecordsFromXls(xls);
 
-        me.loadRemoteData(records);
+        me.loadRemoteData(records, function(receivedRecords) {
+            me.getView().getStore().add(me.mergeRecordArrays(records, receivedRecords));
+        });
     }
 });
