9cce5230ec5a838fa75b90fea3290e09c98df687
DCrow <d_dcrow@outlook.com>
2016-07-27 16:12:59 +0300

    DEV-30590

---
+++ common/src/lib/data/proxy/RestJson.js
@@ -0,0 +1,12 @@
+Ext.define('Ext.lib.data.proxy.RestJson', {
+    extend: 'Ext.data.proxy.Rest',
+    alias: 'proxy.restjson',
+
+    limitParam: '',
+    pageParam: '',
+      
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+}); 
--- common/src/lib/data/proxy/RestJsonLimitless.js
+++ common/src/lib/data/proxy/RestJsonLimitless.js
@@ -1,26 +1,14 @@
 Ext.define('Ext.lib.data.proxy.RestJsonLimitless', {
-	extend : 'Ext.data.proxy.Rest',
-	alias : 'proxy.restjsonlimitless',
+    extend: 'Ext.lib.data.proxy.RestJson',
+    alias: 'proxy.restjsonlimitless',
 
-	limitParam : '',
-	pageParam : '',
-	
-	extraParams: {
-		limitless: true
-	},
-	
-	headers : {
-		'Accept': 'application/json',
-		'Content-Type' : 'application/json'
-	},
-	
-	constructor: function(config){
-		var me = this;
-		if(!config.extraParams){
-			config.extraParams = {};
-		};
-		config.extraParams.limitless = true;
-		
-		me.callParent(arguments);
-	}
+    constructor: function(config){
+        var me = this;
+        if(!config.extraParams){
+            config.extraParams = {};
+        };
+        config.extraParams.limitless = true;
+        
+        me.callParent(arguments);
+    }
 }); 
