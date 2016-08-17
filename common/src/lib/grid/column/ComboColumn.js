05ebf63f3a14f07bd3a095d5958033df2b23340a
DCrow <d_dcrow@outlook.com>
2016-08-17 12:57:23 +0300

    DEV-30957. Fix ComboColumn DisplayValue

--- common/src/lib/grid/column/ComboColumn.js
+++ common/src/lib/grid/column/ComboColumn.js
@@ -143,7 +143,7 @@ Ext.define('Ext.lib.grid.column.ComboColumn', {
                         }
                         return matching == null;
                     });
-                    return matching || v || "";
+                    return matching || (foreignKey ? v : null) || "";
                 },
                 depends: [me.dataIndex],
                 persist: false
