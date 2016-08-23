907eaf3aca8e46f63479a89d60c0aee54de012e3
DCrow <d_dcrow@outlook.com>
2016-08-23 11:27:58 +0300

    DEV-30866

---
+++ common/lib/data/validator/DateValidator.js
@@ -0,0 +1,24 @@
+Ext.define('Lib.data.validator.DateValidator', {
+
+    /**  
+     * Проверяет, что дата начала меньше дата конца
+     * Передать checkNull для проверки на пустые значения
+     * @param {String} dateFrom
+     * @param {String} dateTo
+     * @param {Boolean} checkNull
+     * @return {String/Boolean} Сообщение о не корректных значениях или true 
+     */
+    
+    validateDates: function(dateFrom, dateTo, checkNull){
+        var me = this;
+        if (me.get(dateFrom) && me.get(dateTo)){
+            if (me.get(dateFrom).valueOf() >= me.get(dateTo).valueOf()){
+                return '"Дата до" меньше "Дата от"';
+            } else {
+                return true;
+            }
+        } else if (checkNull) {
+            return 'Поля дат не могут быть пустыми';
+        }
+    }
+});
