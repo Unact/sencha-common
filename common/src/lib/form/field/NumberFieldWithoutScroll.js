72c09bd55202fabe2cc93a8d29397d9f93c4ec17
DCrow <d_dcrow@outlook.com>
2016-09-09 12:08:12 +0300

    DEV-30274

---
+++ common/src/lib/form/field/NumberFieldWithoutScroll.js
@@ -0,0 +1,17 @@
+/**
+ * Класс поля без колесика прокрутки
+ */
+Ext.define('Ext.lib.form.field.NumberFieldWithoutScroll', {
+    extend: 'Ext.form.field.Number',
+    alias: 'widget.numberfieldwithoutscroll',
+    
+    fieldStyle: 'text-align: right;',
+    
+    hideTrigger: true,
+    
+    keyNavEnabled: false,
+    
+    mouseWheelEnabled: false
+    
+});
+
