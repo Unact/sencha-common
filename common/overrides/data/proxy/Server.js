0390679be27182946f0330c6f24ff5fcae344c71
DCrow <d_dcrow@outlook.com>
2016-10-21 15:37:35 +0300

    DEV-31326

--- common/overrides/data/proxy/Server.js
+++ common/overrides/data/proxy/Server.js
@@ -1,19 +1,25 @@
 Ext.define('Ext.overrides.data.proxy.Server', {
-	override : 'Ext.data.proxy.Server',
-	
-	mergeExtraParams : function(extraParams) {
-		var me = this;
-		
-		me.extraParams = Ext.apply(me.extraParams, extraParams);
-	},
+    override: 'Ext.data.proxy.Server',
+    
+    mergeExtraParams: function(extraParams) {
+        var me = this;
+        
+        me.extraParams = Ext.apply(me.extraParams, extraParams);
+    },
 
-	setExtraParamAndRemoveIfNull : function(extraParam, value) {
-		var me = this;
-		
-		if(value!=null){
-			me.setExtraParam(extraParam, value);
-		} else {
-			delete me.extraParams[extraParam];
-		}
-	}
+    setExtraParamAndRemoveIfNull: function(extraParam, value) {
+        var me = this;
+        
+        if(value!=null){
+            me.setExtraParam(extraParam, value);
+        } else {
+            delete me.extraParams[extraParam];
+        }
+    },
+
+    setExtraParamsAndRemoveIfNull: function(extraParams) {
+        for(var extraParam in extraParams){
+            this.setExtraParamAndRemoveIfNull(extraParam, extraParams[extraParam]);
+        }
+    }
 }); 
