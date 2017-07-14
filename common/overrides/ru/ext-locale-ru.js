91cf9c785be4294dd7036e581892a2c0d2ff5f01
DCrow <d_dcrow@outlook.com>
2017-07-14 13:16:00 +0300

    DEV-33734

--- common/overrides/ru/ext-locale-ru.js
+++ common/overrides/ru/ext-locale-ru.js
@@ -1,54 +1,52 @@
-Ext.onReady(function() {
-    Ext.define('Ext.locale.ru.Date', {
-        override: 'Ext.Date',
-        defaultFormat: 'd.m.Y'
-    });
+Ext.define('Ext.locale.ru.Date', {
+    override: 'Ext.Date',
+    defaultFormat: 'd.m.Y'
+});
 
-    Ext.define('Ext.overrides.data.Model', {
-        override: 'Ext.data.Model',
+Ext.define('Ext.overrides.data.Model', {
+    override: 'Ext.data.Model',
 
-        defaultErrorMessage: 'Установлены некорректные значения',
-    });
+    defaultErrorMessage: 'Установлены некорректные значения',
+});
 
-    Ext.define('Ext.locale.ru.grid.RowEditor', {
-        override: 'Ext.grid.RowEditor',
+Ext.define('Ext.locale.ru.grid.RowEditor', {
+    override: 'Ext.grid.RowEditor',
 
-        saveBtnText: 'Сохранить',
-        cancelBtnText: 'Отмена',
-        errorsText: 'Ошибки',
-        dirtyText: 'Вам необходимо сохранить изменения'
-    });
+    saveBtnText: 'Сохранить',
+    cancelBtnText: 'Отмена',
+    errorsText: 'Ошибки',
+    dirtyText: 'Вам необходимо сохранить изменения'
+});
 
-    Ext.define('Ext.overrides.data.field.Field', {
-        override: 'Ext.data.field.Field',
+Ext.define('Ext.overrides.data.field.Field', {
+    override: 'Ext.data.field.Field',
 
-        defaultInvalidMessage: 'Поле заполнено некорректно'
-    });
+    defaultInvalidMessage: 'Поле заполнено некорректно'
+});
 
 
-    Ext.define('Ext.locale.ru.window.MessageBox', {
-        override: 'Ext.window.MessageBox',
+Ext.define('Ext.locale.ru.window.MessageBox', {
+    override: 'Ext.window.MessageBox',
 
-        buttonText: {
-            ok: 'ОК',
-            yes: 'Да',
-            no: 'Нет',
-            cancel: 'Отмена'
-        }
-    });
-
-    if (Ext.util && Ext.util.Format) {
-        Ext.apply(Ext.util.Format, {
-            thousandSeparator: ' ',
-            currencySign: '\u20bd',
-
-            ruMoney: function(v){
-                return Ext.util.Format.currency(v, null, 2, true);
-            },
-
-            withoutDecimal: function(value){
-                return Ext.util.Format.number(value, '0,000');
-            }
-        });
+    buttonText: {
+        ok: 'ОК',
+        yes: 'Да',
+        no: 'Нет',
+        cancel: 'Отмена'
     }
 });
+
+if (Ext.util && Ext.util.Format) {
+    Ext.apply(Ext.util.Format, {
+        thousandSeparator: ' ',
+        currencySign: '\u20bd',
+
+        ruMoney: function(v){
+            return Ext.util.Format.currency(v, null, 2, true);
+        },
+
+        withoutDecimal: function(value){
+            return Ext.util.Format.number(value, '0,000');
+        }
+    });
+}
