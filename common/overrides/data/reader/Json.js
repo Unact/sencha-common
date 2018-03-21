501ccaa5d165369343044329e3bc23fdc0efc08a
DCrow <y.barkovskij@unact.ru>
2018-03-21 16:56:30 +0300

    DEV-35704

---
+++ common/overrides/data/reader/Json.js
@@ -0,0 +1,26 @@
+Ext.define('Ext.overrides.data.reader.Json', {
+    override: 'Ext.data.reader.Json',
+
+    extractData: function(root, readOptions) {
+        var recordName = this.getRecord(),
+            data = [],
+            length, i;
+
+        if (recordName) {
+
+            if (Ext.isArray(root)) {
+                length = root.length;
+            } else {
+                length = 1;
+                root = [root];
+            }
+
+            for (i = 0; i < length; i++) {
+                data[i] = root[i][recordName];
+            }
+        } else {
+            data = root;
+        }
+        return this.callParent([data, readOptions]);
+    }
+});
