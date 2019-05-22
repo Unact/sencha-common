e7ef5cb79f6d66ce1d39f6ac5338b8c0bdbc2663
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2019-05-22 17:23:10 +0300

    DEV-38509

--- common/overrides/data/field/Number.js
+++ common/overrides/data/field/Number.js
@@ -1,6 +1,8 @@
 Ext.define('Ext.overrides.data.field.Number', {
     override: 'Ext.data.field.Number',
 
+    stripRe: /[^\d.-]/g,
+
     // Since default JS decimalSeparator is '.', change all ',' to '.'
     // To do: Add decimal and thousand separators
     parse: function(v) {
