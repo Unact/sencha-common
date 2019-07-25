c63c626a07912c386e4a6bffef4e0e68aae55719
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2019-07-25 13:47:43 +0300

    DEV-38687

---
+++ common/src/lib/sp/View.js
@@ -0,0 +1,56 @@
+Ext.define('Ext.lib.sp.View', {
+    extend: 'Ext.lib.singlegrid.View',
+    alias: 'widget.sp',
+
+    config: {
+        enabledButtons: ['save', 'refresh'],
+    },
+
+    columns: [{
+        text: 'Название',
+        dataIndex: 'name',
+        width: 150
+    }, {
+        text: 'Значение',
+        dataIndex: 'value',
+        width: 250,
+        renderer: function(v, metaData, rec) {
+            return rec.get('value') ? rec.get('value_name') : null;
+        },
+        getEditor: function(record) {
+            if (record.get('type') === app.model.Sp.STRING_TYPE) {
+                return Ext.create('Ext.grid.CellEditor', {
+                    field: Ext.create('Ext.form.field.Text', {
+                        listeners: {
+                            change: function(field, newVal) {
+                                record.set('value_name', newVal);
+                            }
+                        }
+                    })
+                });
+            }
+
+            return Ext.create('Ext.grid.CellEditor', {
+                field: Ext.create('Ext.form.field.ComboBox', {
+                    displayField: 'name',
+                    valueField: 'id',
+                    forceSelection: true,
+                    queryCaching: false,
+                    minChars: 1,
+                    queryParam: 'q[name_cont]',
+                    listeners: {
+                        beforequery: function(queryPlan) {
+                            queryPlan.combo.getStore().getProxy().setExtraParam('q[sp_tp_eq]', record.get('id'));
+                        },
+                        select: function(combo, selectedRec) {
+                            record.set('value_name', selectedRec.get('name'));
+                        }
+                    },
+                    bind: {
+                        store: '{spValues}'
+                    }
+                })
+            });
+        }
+    }]
+});
