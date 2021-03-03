7ffe882d16d80998eca069690fcb268117695d15
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2021-03-03 12:08:06 +0300

    DEV-40189

--- common/overrides/grid/column/Column.js
+++ common/overrides/grid/column/Column.js
@@ -14,5 +14,53 @@ Ext.define('Ext.overrides.grid.column.Column', {
         }
 
         grid.view.refresh();
-    }
+    },
+
+    applyColumnState: function(state, storeState) {
+        var me = this,
+            sorter = me.getSorter(),
+            stateSorters = storeState && storeState.sorters,
+            len, i, savedSorter, mySorterId;
+        // If we have been configured with a sorter, then there SHOULD be a sorter config
+        // in the storeState with a corresponding ID from which we must restore our sorter's state.
+        // (The only state we can restore is direction).
+        // Then we replace the state entry with the real sorter. We MUST do this because the sorter
+        // is likely to have a custom sortFn.
+        if (sorter && stateSorters && (len = stateSorters.length)) {
+            mySorterId = sorter.getId();
+            for (i = 0; !savedSorter && i < len; i++) {
+                if (stateSorters[i].id === mySorterId) {
+                    sorter.setDirection(stateSorters[i].direction);
+                    stateSorters[i] = sorter;
+                    break;
+                }
+            }
+        }
+        // apply any columns
+        me.applyColumnsState(state.columns);
+        // Only state properties which were saved should be restored.
+        // (Only user-changed properties were saved by getState)
+        if (state.hidden != null) {
+            me.hidden = state.hidden;
+        }
+        if (state.locked != null) {
+            me.locked = state.locked;
+        }
+        if (state.sortable != null) {
+            me.sortable = state.sortable;
+        }
+
+        /// FIX
+        /// If a column has child columns its width cannot be restored from state
+        /// since its width is calculated from child widths
+        if (!this.columnManager) {
+            if (state.width != null) {
+                me.flex = null;
+                me.width = state.width;
+            } else if (state.flex != null) {
+                me.width = null;
+                me.flex = state.flex;
+            }
+        }
+    },
 });
